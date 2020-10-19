462cc72f8c30a42564f9871df6aa99ecba92eb24
James Goff <james_goff88@outlook.com>
2020-10-18 19:59:35 -0500

    initial commit

---
+++ src/components/layout/Preloader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Preloader = () => {
+	return (
+		<div className="progress cyan lighten-4">
+			<br />
+			<div className="indeterminate cyan" />
+		</div>
+	);
+};
+
+export default Preloader;
